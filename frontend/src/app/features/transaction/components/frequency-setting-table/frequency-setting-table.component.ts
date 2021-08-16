930274606fa90e4680277d5937361e0ec5745703
CBTC\AmberWu <amberwu@chipbond.com.tw>
2021-08-16 11:29:57 +0800

    refactor: 拆分 txn-create頁面內的job setting 及其他
    1. txn-create.scss原始碼重整
    2. txn-basic-info (AP Booking欄位)及 frequency-setting-table(Time Zone ID欄位)，將原本的autoComplete替換成p-dropdown

--- frontend/src/app/features/transaction/components/frequency-setting-table/frequency-setting-table.component.ts
+++ frontend/src/app/features/transaction/components/frequency-setting-table/frequency-setting-table.component.ts
@@ -13,6 +13,12 @@ import {
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FormGroupState } from 'ngrx-forms';
 
+interface timeZoneInfo {
+  name: string,
+  code: string
+}
+
+
 @Component({
   selector: 'app-frequency-setting-table',
   templateUrl: './frequency-setting-table.component.html',
@@ -24,7 +30,8 @@ export class FrequencySettingTableComponent implements OnInit {
   @Input() options!: string[];
   @Input() submittedValue!: DynamicFormValue | undefined;
   @Output() addFrequencySettingEvent = new EventEmitter();
-  timeZoneIds: string[] = [];
+
+  timeZoneIds:timeZoneInfo[];
   accordionStates = false;
 
   stateOptionsAccordion = [
@@ -32,16 +39,21 @@ export class FrequencySettingTableComponent implements OnInit {
     { label: 'N', value: false },
   ];
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    this.timeZoneIds = [
+      {name: 'Taipei Standard Time', code: ''},
+      {name: 'Japan Standard Time', code: ''}
+    ];
+  }
 
   ngOnInit(): void {
     console.log(this.formState);
   }
 
-  filterTimeZone(inputtedCustNameEvent: any): void {
-    let query = inputtedCustNameEvent.query;
-    this.timeZoneIds = ['Taipei Standard Time', 'Japan Standard Time'];
-  }
+  // filterTimeZone(inputtedCustNameEvent: any): void {
+  //   let query = inputtedCustNameEvent.query;
+  //   this.timeZoneIds = ['Taipei Standard Time', 'Japan Standard Time'];
+  // }
 
   toggleAccordion(event: any): void {
     this.accordionStates = !this.accordionStates;
--- frontend/src/app/features/transaction/containers/job-setting/job-setting.component.ts
+++ frontend/src/app/features/transaction/containers/job-setting/job-setting.component.ts
@@ -1,30 +1,15 @@
-import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Handsontable from 'handsontable';
 import { HandsontableHooks,rowRequireValidator } from 'src/app/shared/handsontable/handsontable.component';
 
-
 @Component({
-  selector: 'app-txn-create-job',
-  templateUrl: './txn-create-job.component.html',
-  styleUrls: ['./txn-create-job.component.scss']
+  selector: 'app-job-setting',
+  templateUrl: './job-setting.component.html',
+  styleUrls: ['./job-setting.component.scss']
 })
-export class TxnCreateJobComponent implements OnInit {
-
+export class JobSettingComponent implements OnInit {
   allJobDLLCondition!: FormGroup;
-  // jobForm!: FormGroup;
-  // get jobName() {
-  //   return this.jobForm.get('jobName');
-  // }
-  // get maxRetry() {
-  //   return this.jobForm.get('maxRetry');
-  // }
-  // get DLLSeq() {
-  //   return this.jobForm.get('DLLSeq');
-  // }
-  // DLLSeqs!: string[];
-  jobDLLCondition!: FormGroup;
-
 
   tableHeaders: TableHeaders = {
     ConditionID: 'Condition ID',
@@ -83,28 +68,11 @@ export class TxnCreateJobComponent implements OnInit {
     colHeaders: Object.values(this.tableHeaders),
   };
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     const allJobConditionInfo = () => {
-      this.allJobDLLCondition = this.formBuilder.group({
-        ConditionID: [, Validators.required],
-        ConditionName: [, Validators.required],
-        ConditionValue: [, Validators.required],
-        Memo: [, Validators.required],
-      });
-    };
-
-    // const jobFormInfo = () => {
-    //   this.jobForm = this.formBuilder.group({
-    //     jobName: [, Validators.required],
-    //     maxRetry: [, Validators.required],
-    //     DLLSeq: [, Validators.required],
-    //   });
-    // };
-
-    const jobConditionInfo = () => {
-      this.jobDLLCondition = this.formBuilder.group({
+      this.allJobDLLCondition = this.fb.group({
         ConditionID: [, Validators.required],
         ConditionName: [, Validators.required],
         ConditionValue: [, Validators.required],
@@ -113,21 +81,7 @@ export class TxnCreateJobComponent implements OnInit {
     };
 
     allJobConditionInfo();
-    // jobFormInfo();
-    jobConditionInfo();
   }
-
-
-  // filterDLLSeqs(inputtedCustNameEvent: any): void {
-  //   let query = inputtedCustNameEvent.query;
-  //   this.DLLSeqs = [
-  //     '0000000003',
-  //     '0000000004',
-  //     '0000000012',
-  //     '0000000013',
-  //     '0000000020'
-  //   ]
-  // }
 
 }
 
@@ -137,10 +91,6 @@ interface TableDataSchema {
   ConditionName: string | null;
   ConditionValue: string | null;
   Memo: string | null;
-  // ReelQty: number | null;
-  // TapingContent: string | null;
-  // TapingQty: number | null;
-  // CarrierTapePN: string | null;
 }
 
 type TableHeaders = Record<keyof TableDataSchema, string>;
